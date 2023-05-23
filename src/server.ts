b46857c3275202f1708aab27831b77e0c8289f12
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-05-22 23:27:11 -0300

    Feat:Estrutura da aplicação - Configurando ESLint

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
 const app = fastify()
 
 // GET, POST, PUT, PATCH, DELETE
 
-app.get('/hello', ()=>{
-    return "Hello word Node.js"
+app.get('/hello', () => {
+  return 'Hello word Node.js'
 })
 
-app.listen({
+app
+  .listen({
     port: 3333,
-}).then(() =>{
-    console.log('HTTP Server Running!');
-})
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
